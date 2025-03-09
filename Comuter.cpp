3ebcce5d42bb706dd08739ba4c2b87017320d23c
darius <nguyenleanhdung0903@gmail.com>
2025-03-09 21:40:33 +1030

    prac1

---
+++ Comuter.cpp
@@ -0,0 +1,7 @@
+#include "Computer.h"
+
+Computer::Computer(string name) : name(name) {}
+
+char Computer::makeMove() { return 'R'; }
+
+string Computer::getName() { return name; }
---
+++ Referee.cpp
@@ -0,0 +1,19 @@
+#include "Referee.h"
+
+#include "Computer.h"
+#include "Human.h"
+#include "Player.h"
+using namespace std;
+#include <iostream>
+int main() {
+  Human human("Khanh");
+  Computer comp;
+  Referee ref;
+  Player* winner = ref.refGame(&human, &comp);
+  if (winner == nullptr) {
+    cout << "It's a Tie." << endl;
+  } else {
+    cout << winner->getName() << " Wins." << endl;
+  }
+  return 0;
+}
